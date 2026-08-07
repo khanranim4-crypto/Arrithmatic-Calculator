086bbfddc9e8a45294eff470fe60da05c3063869
khanranim4-crypto <khanranim4@gmail.com>
2026-08-08 09:26:02 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrithmatic_Calculator
+{
+  
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Addition Claculator");
+
+                //Ask the user to enter the first number
+                Console.Write("Enter the first number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                //Ask the user to enter the second number
+                Console.Write("Enter the second number: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int result = a + b;
+
+                Console.WriteLine("The total is: " + result);
+
+                Console.Read();
+            }
+        }
+    
+}
